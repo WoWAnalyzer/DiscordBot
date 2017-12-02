c38e9d72fd44c87a22f853dd26c75295e884c46a
Martijn Hols <martijnhols@hotmail.com>
2017-12-02 22:50:53 +0100

    Setup Sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 // noinspection JSFileReferences
 import Discord from 'discord.js';
+import Raven from 'raven';
 
 import onReady from './onReady';
 import onMessage from './onMessage';
+
+if (process.env.NODE_ENV === 'production') {
+  Raven.config('https://669adafa75124f91bff5754d9c34b13a:b07f6d83af184b18ac2a554d0509e2f6@sentry.io/253783', {
+    captureUnhandledRejections: true,
+  }).install();
+}
 
 console.log('Token:', process.env.DISCORD_TOKEN);
 
