03d13deb19bb3325696514cb3094ca2da24624e2
Martijn Hols <martijnhols@hotmail.com>
2017-12-07 23:55:12 +0100

    Run Docker container in production mode

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ COPY build /usr/src/wowanalyzer/discordbot/build/
 COPY node_modules /usr/src/wowanalyzer/discordbot/node_modules/
 COPY package.json /usr/src/wowanalyzer/discordbot/
 
+ENV NODE_ENV=production
 USER node
 CMD ["npm", "run", "prod"]
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Raven from 'raven';
 import main from './main';
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('Sentry is ENABLED');
   Raven.config('https://669adafa75124f91bff5754d9c34b13a:b07f6d83af184b18ac2a554d0509e2f6@sentry.io/253783', {
     captureUnhandledRejections: true,
   }).install();
