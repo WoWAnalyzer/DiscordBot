892a97c27412acbad9b0d830f7bfd592ce6e8983
Martijn Hols <martijnhols@hotmail.com>
2018-03-23 16:45:56 +0100

    Update dependencies to hopefully fix the silent quiting after a random period of time

--- src/onReady.js
+++ src/onReady.js
@@ -1,4 +1,4 @@
 export default function onReady(client) {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame('WoWAnalyzer.com');
+  client.user.setActivity('WoWAnalyzer.com');
 }
