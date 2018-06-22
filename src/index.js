2c577e6feb9b913234623b987a86ee85511e8503
Martijn Hols <git@martijnhols.nl>
2018-06-22 18:54:09 +0200

    New Sentry doesn't need this

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import main from './main';
 
 if (process.env.NODE_ENV === 'production') {
   console.log('Sentry is ENABLED');
-  Raven.config('https://669adafa75124f91bff5754d9c34b13a:b07f6d83af184b18ac2a554d0509e2f6@sentry.io/253783', {
-    captureUnhandledRejections: true,
-  }).install();
+  Raven.config('https://669adafa75124f91bff5754d9c34b13a@sentry.io/253783').install();
 }
 
 main(process.env.DISCORD_TOKEN);
