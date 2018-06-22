51db0b26679af2fc7113a74b8a5d3c3ebf6f37de
Martijn Hols <git@martijnhols.nl>
2018-06-22 19:18:13 +0200

    Clean up onMessage test

--- src/onMessage.test.js
+++ src/onMessage.test.js
@@ -1,5 +1,5 @@
-import onMessage  from './onMessage';
-import { putOnCooldown, checkHistoryPurge } from './memoryHistory';
+import onMessage from './onMessage';
+import { checkHistoryPurge, putOnCooldown } from './memoryHistory';
 
 jest.mock('./getFights');
 jest.mock('./memoryHistory');
@@ -25,64 +25,55 @@ describe('onMessage', () => {
       embeds: [],
       ...props,
     });
+    console.log = jest.fn();
   });
 
-  it('responds to a regular WCL link', () => {
+  it('responds to a regular WCL link', async () => {
     const message = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE');
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE');
   });
-  it('responds to a message with padding text', () => {
+  it('responds to a message with padding text', async () => {
     const message = createMessage({
       content: 'Please review: https://www.warcraftlogs.com/reports/PROPERREPORTCODE. Thanks!',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE');
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE');
   });
-  it('does not respond to a message with an invalid or private report', () => {
+  it('does not respond to a message with an invalid or private report', async () => {
     const message = createMessage({
       content: 'Please review: https://www.warcraftlogs.com/reports/AWRONGREPORTCODE. Thanks!',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).not.toHaveBeenCalled();
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).not.toHaveBeenCalled();
   });
-  it('ignores messages with multiple links', () => {
+  it('ignores messages with multiple links', async () => {
     const message = createMessage({
       content: 'https://www.warcraftlogs.com/reports/AB1CDEf2G3HIjk4L and https://www.warcraftlogs.com/reports/aaaaaaaaaaaaaaaa',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).not.toHaveBeenCalled();
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).not.toHaveBeenCalled();
   });
-  it('requires domain', () => {
+  it('requires domain', async () => {
     const message = createMessage({
       content: '/reports/AB1CDEf2G3HIjk4L',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).not.toHaveBeenCalled();
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).not.toHaveBeenCalled();
   });
-  it('responds to bots (like the WCL webhook)', () => {
+  it('responds to bots (like the WCL webhook)', async () => {
     const message = createMessage({
       author: {
         bot: true,
@@ -90,95 +81,82 @@ describe('onMessage', () => {
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalled();
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalled();
   });
-  it('preselects fight and player', () => {
+  it('preselects fight and player', async () => {
     const message = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE/#fight=11&source=75&type=healing',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE/11-Mythic+Sisters+of+the+Moon+-+Kill+(5:16)/75-Zerotorescue');
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE/11-Mythic+Sisters+of+the+Moon+-+Kill+(5:16)/75-Zerotorescue');
   });
-  it('preselects fight when it\'s alone', () => {
+  it('preselects fight when it\'s alone', async () => {
     const message = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE/#fight=11',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE/11-Mythic+Sisters+of+the+Moon+-+Kill+(5:16)');
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE/11-Mythic+Sisters+of+the+Moon+-+Kill+(5:16)');
   });
-  it('preselects fight when it\'s last', () => {
+  it('preselects fight when it\'s last', async () => {
     const message = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE/#fight=last',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE/42-Mythic+Fallen+Avatar+-+Wipe+14+(3:49)');
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE/42-Mythic+Fallen+Avatar+-+Wipe+14+(3:49)');
   });
-  it('ignores preselected player if fight isn\'t provided', () => {
+  it('ignores preselected player if fight isn\'t provided', async () => {
     const message = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE/#source=75&type=healing',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE');
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalledWith('https://wowanalyzer.com/report/PROPERREPORTCODE');
   });
-  it('ignores reports with advanced filters', () => {
+  it('ignores reports with advanced filters', async () => {
     const message1 = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE/#source=75&type=healing&start=2356367&end=2381131',
     });
+
+    await onMessage({}, message1);
+
+    expect(message1.channel.send).not.toHaveBeenCalled();
+
     const message2 = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE/#fight=11&source=75&start=2356367&end=2381131&view=events&pins=2%24Off%24%23244F4B%24auras-gained%24-1%240.0.0.Any%240.0.0.Any%24true%240.0.0.Any%24true%2431821%24true%24true',
     });
+
+    await onMessage({}, message2);
+
+    expect(message2.channel.send).not.toHaveBeenCalled();
+
     const message3 = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE/#source=75&fight=last&type=healing&phase=1',
     });
-    expect.assertions(3);
-    return Promise.all([
-      onMessage({}, message1)
-        .then(() => {
-          expect(message1.channel.send).not.toHaveBeenCalled();
-        }),
-      onMessage({}, message2)
-        .then(() => {
-          expect(message2.channel.send).not.toHaveBeenCalled();
-        }),
-      onMessage({}, message3)
-        .then(() => {
-          expect(message2.channel.send).not.toHaveBeenCalled();
-        }),
-    ]);
+
+    await onMessage({}, message3);
+
+    expect(message2.channel.send).not.toHaveBeenCalled();
   });
-  it('puts a report on cooldown after sending it', () => {
+  it('puts a report on cooldown after sending it', async () => {
     const message = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE',
     });
 
-    expect.assertions(2);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalled();
-        expect(putOnCooldown).toHaveBeenCalledWith(12345, 'PROPERREPORTCODE');
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalled();
+    expect(putOnCooldown).toHaveBeenCalledWith(12345, 'PROPERREPORTCODE');
   });
-  describe('Scoped / Nested block', () => {
+  describe('memoryHistory', async () => {
     const memoryHistory = require('./memoryHistory');
     beforeEach(() => {
       memoryHistory.isOnCooldown = jest.fn(() => true);
@@ -186,41 +164,35 @@ describe('onMessage', () => {
     afterEach(() => {
       memoryHistory.isOnCooldown = jest.fn();
     });
-    it('ignores a report already on cooldown', () => {
+    it('ignores a report already on cooldown', async () => {
       const message = createMessage({
         content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE',
       });
 
-      expect.assertions(1);
-      return onMessage({}, message)
-        .then(() => {
-          expect(message.channel.send).not.toHaveBeenCalled();
-        });
+      await onMessage({}, message);
+
+      expect(message.channel.send).not.toHaveBeenCalled();
     });
   });
-  it('triggers the history purge check', () => {
+  it('triggers the history purge check', async () => {
     const message = createMessage({
       content: 'https://www.warcraftlogs.com/reports/PROPERREPORTCODE',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(checkHistoryPurge).toHaveBeenCalled();
-      });
+    await onMessage({}, message);
+
+    expect(checkHistoryPurge).toHaveBeenCalled();
   });
-  it('handles emojis gracefully (does not trigger an unhandled promise rejection)', () => {
+  it('handles emojis gracefully (does not trigger an unhandled promise rejection)', async () => {
     const message = createMessage({
       content: 'pepe_oops:346683709187031041',
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).not.toHaveBeenCalled();
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).not.toHaveBeenCalled();
   });
-  it('also responds to urls in embeds', () => {
+  it('also responds to urls in embeds', async () => {
     const message = createMessage({
       embeds: [
         {
@@ -229,10 +201,8 @@ describe('onMessage', () => {
       ],
     });
 
-    expect.assertions(1);
-    return onMessage({}, message)
-      .then(() => {
-        expect(message.channel.send).toHaveBeenCalled();
-      });
+    await onMessage({}, message);
+
+    expect(message.channel.send).toHaveBeenCalled();
   });
 });
