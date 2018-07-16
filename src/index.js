59ce495bc0997f55047c2e9a22193cc6507e1bc4
Martijn Hols <git@martijnhols.nl>
2018-07-16 18:22:52 +0200

    Catch unahndled rejections

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import main from './main';
 
 if (process.env.NODE_ENV === 'production') {
   console.log('Sentry is ENABLED');
-  Raven.config('https://669adafa75124f91bff5754d9c34b13a@sentry.io/253783').install();
+  // I thought Sentry caught unhandled rejections by default now, but not so according to https://docs.sentry.io/clients/node/usage/#promises + it didn't catch some rejections
+  Raven.config('https://669adafa75124f91bff5754d9c34b13a@sentry.io/253783', {
+    captureUnhandledRejections: true
+  }).install();
 }
 
 main(process.env.DISCORD_TOKEN);
