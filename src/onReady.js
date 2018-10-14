c390a1d1f9361634bae672d56ef5e6ce6d95f708
Martijn Hols <git@martijnhols.nl>
2018-10-14 15:42:42 +0200

    Report amount of servers on startup

--- src/onReady.js
+++ src/onReady.js
@@ -1,4 +1,5 @@
 export default function onReady(client) {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity('WoWAnalyzer.com');
+  console.log('Currently in', client.guilds.size, 'servers');
 }
