902ca7ae963c5fec34092700b10ad305e1562b9a
Martijn Hols <git@martijnhols.nl>
2021-09-30 16:18:29 +0200

    Update Sentry DSN

--- src/init.js
+++ src/init.js
@@ -12,7 +12,7 @@ loadDotEnv(appDirectory);
 if (process.env.NODE_ENV === 'production') {
   console.log('Sentry is ENABLED');
   // I thought Sentry caught unhandled rejections by default now, but not so according to https://docs.sentry.io/clients/node/usage/#promises + it didn't catch some rejections
-  Raven.config('https://669adafa75124f91bff5754d9c34b13a@sentry.io/253783', {
+  Raven.config('https://669adafa75124f91bff5754d9c34b13a@o105799.ingest.sentry.io/253783', {
     captureUnhandledRejections: true
   }).install();
 }
