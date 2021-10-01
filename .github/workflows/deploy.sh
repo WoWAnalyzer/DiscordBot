8221d42d18bcdd8fc4345b9db319e22a587fa9ac
Martijn Hols <git@martijnhols.nl>
2021-10-01 15:03:55 +0200

    Migrate CD to GitHub Actions

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,16 @@
+export REPO=wowanalyzer/wowanalyzer-discordbot;
+export DEPLOY_TAG=$(
+  if [ "${GITHUB_REF##*/}" == "master" ]; then
+    echo "latest";
+  else
+    echo ${GITHUB_REF##*/};
+  fi | sed -r 's/\//-/g'
+);
+
+echo "> Create a Docker image for this specific build. This allows us to go back to a particular build at any time, and makes it possible to deploy without rebuilding by just re-tagging the image.";
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
+docker tag wowanalyzer-discordbot $REPO:$DOCKER_BUILD_TAG;
+docker push $REPO:$DOCKER_BUILD_TAG;
+docker tag $REPO:$DOCKER_BUILD_TAG $REPO:$DEPLOY_TAG;
+docker push $REPO:$DEPLOY_TAG;
