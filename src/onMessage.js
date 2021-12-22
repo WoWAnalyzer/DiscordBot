571a1ae1ca16dc359346301d4296a0777f1a4c3c
trevorm4 <trevorm4@illinois.edu>
2021-12-22 14:00:28 -0500

    Removed comment

--- src/onMessage.js
+++ src/onMessage.js
@@ -66,7 +66,6 @@ export default function onMessage(client, msg) {
   const urls = getUrlsFromMessage(msg);
   if (!urls) {
     // Ignore messages without links (for obvious reasons).
-    // Ignore messages with more than 1 link. This might be revised later, but for now it seems likely that messages with multiple links may not be requests for log analysis. Ofc this is a very simplified requirement and I think it can be removed once we ignore repeated report links within a certain period of time, as that should be enough to prevent spammy, annoying responses.
     return Promise.resolve();
   }
 
@@ -118,7 +117,7 @@ export default function onMessage(client, msg) {
         if(messageToSend.length === 0) {
           return;
         }
-        
+
         msg.channel.send(messageToSend);
 
         values.map((url) => {
